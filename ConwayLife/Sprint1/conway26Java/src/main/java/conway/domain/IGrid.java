6e7936ca7cf57e9307fef45ebe890db480de91d4
Gabriele <dotigabriele@gmail.com>
2026-03-08 13:18:25 +0100

    Modify I/Cell I/Grid

--- ConwayLife/Sprint1/conway26Java/src/main/java/conway/domain/IGrid.java
+++ ConwayLife/Sprint1/conway26Java/src/main/java/conway/domain/IGrid.java
@@ -3,20 +3,20 @@ package main.java.conway.domain;
 public interface IGrid {
 
     /* Returns the number of rows of the grid */
-    public int getRows();
+    int getRows();
 
     /* Returns the number of columns of the grid */
-    public int getCols();
+    int getCols();
 
     /* Returns the cell located at the given position */
-    public ICell getCell(int row, int column);
+    ICell getCell(int row, int column);
     
     /* Returns the current state of the cell */
-    public boolean isCellAlive(int row, int column);
+    boolean isCellAlive(int row, int column);
 
     /* Sets the state of the cell at the given position */
-    public void setCellState(int row, int column, boolean alive);
+    void setCellState(int row, int column, boolean alive);
     
     /* Set the grid ad the initial state */
-    public void reset();
+    void reset();
 }
